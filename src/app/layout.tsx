bd615b3cbe0623576772f218d0a3208dd29e51bd
riadh <r.benchouche1@gmail.com>
2024-10-28 12:58:58 +0100

    fix fav icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,9 +32,6 @@ export const metadata: Metadata = {
       },
     ],
   },
-  verification: {
-    // google: 'rF1p7hrWwW_kkswltukk9fUBQrOy2VQAx8P7X9htbwc',
-  },
 }
 
 export default function RootLayout({
@@ -45,6 +42,21 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/favicon-48x48.png"
+          sizes="48x48"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
         <link rel="preconnect" href="https://api.fontshare.com" />
         <link
           rel="stylesheet"
